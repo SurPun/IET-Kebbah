9173b4092dfbc3491c6b45c711eb3b06d2ce0754
Suraj Pun <s_pun@outlook.com>
2022-11-28 17:35:10 +0000

    Video-training Pages

---
+++ pages/training.js
@@ -0,0 +1,7 @@
+import React from "react";
+// Show part two intro
+// Btn to link to another
+
+export default function Training() {
+  return <div>Training</div>;
+}
---
+++ pages/training/intro.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ButtonCom from "../../components/ButtonCom";
+// Show part two intro
+// Btn to link to another
+
+export default function TrainingIntro() {
+  return (
+    <>
+      <h1>PART TWO IMMERSIVE TRAINING</h1>
+      <h2>Shane View</h2>
+      <p>
+        You are about to start your immersive exoerience. Please ensure you are
+        ready. Once the training starts you will <b>NOT</b> be able to go back.
+      </p>
+
+      <h3 className="text-red-700">Warning!</h3>
+      <p>
+        You may find the following footage destressing! Please be adviced that
+        the following footage is played by actors.
+      </p>
+      <ButtonCom
+        btnName={"I'm ready to start training"}
+        btnLink="shane-family"
+      />
+    </>
+  );
+}
