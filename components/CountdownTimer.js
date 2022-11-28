880d5313ab1702c5a85354060e07846eb5b4c399
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-11-28 17:31:13 +0000

    countdown timer

--- components/CountdownTimer.js
+++ components/CountdownTimer.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 export default function CountdownTimer() {
   const [counter, setCounter] = useState(30);
 
+
   useEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
   }, [counter]);
