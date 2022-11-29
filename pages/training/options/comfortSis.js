e809f6dafdeaf4a919cbbdc92008858def619baf
Suraj Pun <s_pun@outlook.com>
2022-11-29 12:59:00 +0000

    Added training choices

---
+++ pages/training/options/comfortSis.js
@@ -0,0 +1,15 @@
+export default function ComfortSis() {
+  return (
+    <>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/HDd2u8szPhE?autoplay=1&controls=0&rel=0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope"
+        allowfullscreen
+      ></iframe>
+    </>
+  );
+}
--- pages/training/shane-life.js
+++ pages/training/shane-life.js
@@ -1,17 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ButtonCom from "../../components/ButtonCom";
+import Image from "next/image";
+import TrainingOptions from "../../components/TrainingOptions";
 
 // -------------------------------------
-// Render video
-// until options are displayed
-// Link options
-// On options - display sound and display timer
+// - [x] Render video
+// - [x] until options are displayed
+// - [x] Link options
+// - [ ] On options - display sound and display timer
 
 export default function ShaneLife() {
   return (
     <>
-      <h2>ShaneLife</h2>
+      {/* Google Drive Src */}
+
       <iframe
+        src="https://drive.google.com/file/d/1fCLb07xJsr13tN0H1_plL8LO2hI_FXQv/preview?control=0"
+        width="640"
+        height="480"
+        allow="autoplay"
+      ></iframe>
+      {/* Youtube Src */}
+      {/* <iframe
         width="560"
         height="315"
         src="https://www.youtube-nocookie.com/embed/zQ6jTErlu4k?autoplay=1&controls=0&rel=0"
@@ -19,7 +30,10 @@ export default function ShaneLife() {
         frameborder="0"
         allow="accelerometer; clipboard-write; encrypted-media; gyroscope"
         allowfullscreen
-      ></iframe>
+      ></iframe> */}
+
+      {/* Options Component*/}
+      <TrainingOptions />
     </>
   );
 }
