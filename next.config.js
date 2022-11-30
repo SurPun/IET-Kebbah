b11bcd4e790e63bc9b469da8fab6c9a740176881
Manoela <mo.tasheva@gmail.com>
2022-11-30 17:38:44 +0000

    env file

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true
 };
 
-// import * as dotenv from "dotenv";
-// dotenv.config();
+import * as dotenv from "dotenv";
+dotenv.config();
 
-modeule.exports = nextConfig;
+module.exports = nextConfig;
