a18c90923e9d0ea5196b393850d1d6e71a909f17
Manoela <mo.tasheva@gmail.com>
2022-12-05 15:44:49 +0000

    Navbar styled

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'light-orange': "#e99510",
+        'dim-gray': "#C9D0D7",
         'dark-green': '#1A414B',
         'light-green': '#3C8880',
         'dim-green': '#4B7373',
