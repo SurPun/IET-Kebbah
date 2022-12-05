09f1ff4afa433819a19f673e1152a6cc66bc3455
Abbbyccc <abbychien@hotmail.com>
2022-12-05 12:50:21 +0000

    add head to pages

--- pages/survey-two/introduction.js
+++ pages/survey-two/introduction.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ButtonCom from "../../components/ButtonCom";
+import HeadComp from "../../components/HeadComp";
 
 export default function Introduction() {
   return (
     <>
-      <h1>End of immersive training</h1>
-      <h2>PART THREE Survey II </h2>
-      <p>Police view scenario</p>
-      <ButtonCom btnName={"To Survey"} btnLink={"scenario"} />
+      <HeadComp title={"Survey II Intro"} />
+      <main>
+        <h1>End of immersive training</h1>
+        <h2>PART THREE Survey II </h2>
+        <p>Police view scenario</p>
+        <ButtonCom btnName={"To Survey"} btnLink={"scenario"} />
+      </main>
     </>
   );
 }
--- pages/survey-two/scenario.js
+++ pages/survey-two/scenario.js
@@ -1,53 +1,57 @@
 import { useState } from "react";
 import ButtonCom from "../../components/ButtonCom";
 import ScenarioTranscript from "../../public/textFiles/ScenarioTranscript";
+import HeadComp from "../../components/HeadComp";
 
 export default function Scenario() {
   const [showTranscript, setTranscript] = useState(false);
 
   return (
     <>
-      <h1>Same Scenario - Police - from PART I</h1>
+      <HeadComp title={"Survey II Scenario"} />
+      <main>
+        <h1>Same Scenario - Police - from PART I</h1>
 
-      {showTranscript ? (
-        <div>
-          <ScenarioTranscript />
-          <ButtonCom
-            btnName={"Start Survey"}
-            btnLink="questions" // SENDS US TO QUESTIONS
-          />
+        {showTranscript ? (
+          <div>
+            <ScenarioTranscript />
+            <ButtonCom
+              btnName={"Start Survey"}
+              btnLink="questions" // SENDS US TO QUESTIONS
+            />
 
-          <ButtonCom
-            btnName={"See Video"}
-            BtnOnClick={() => {
-              setTranscript(false);
-            }}
-          />
-        </div>
-      ) : (
-        <div>
-          <iframe
-            src="https://geo.dailymotion.com/player/xaygu.html?video=x8fx226"
-            allow="autoplay; fullscreen; picture-in-picture"
-            allowfullscreen
-            frameborder="0"
-            width="640"
-            height="360"
-            className="pointer-events-none"
-          ></iframe>
-          <ButtonCom
-            btnName={"Start Survey"}
-            btnLink="questions" // SENDS US TO QUESTIONS
-          />
+            <ButtonCom
+              btnName={"See Video"}
+              BtnOnClick={() => {
+                setTranscript(false);
+              }}
+            />
+          </div>
+        ) : (
+          <div>
+            <iframe
+              src="https://geo.dailymotion.com/player/xaygu.html?video=x8fx226"
+              allow="autoplay; fullscreen; picture-in-picture"
+              allowfullscreen
+              frameborder="0"
+              width="640"
+              height="360"
+              className="pointer-events-none"
+            ></iframe>
+            <ButtonCom
+              btnName={"Start Survey"}
+              btnLink="questions" // SENDS US TO QUESTIONS
+            />
 
-          <ButtonCom
-            btnName={"See Transcript"}
-            BtnOnClick={() => {
-              setTranscript(true);
-            }}
-          />
-        </div>
-      )}
+            <ButtonCom
+              btnName={"See Transcript"}
+              BtnOnClick={() => {
+                setTranscript(true);
+              }}
+            />
+          </div>
+        )}
+      </main>
     </>
   );
 }
