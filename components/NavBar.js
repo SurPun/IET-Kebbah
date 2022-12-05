afd40e274da0d9575f6c46e92a1250f34fbc4163
Manoela <mo.tasheva@gmail.com>
2022-12-05 15:56:17 +0000

    Navbar colours

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <header className="list-none flex flex-wrap items-center justify-between mx-auto w-full bg-dim-gray">
+    <header className="list-none flex flex-wrap items-center justify-between mx-auto w-full bg-black">
       <h3>
         <Link href="/">
           <Image
-            className="p-1.5 hover:p-1"
+            className="p-1.5 invert hover:p-1"
             src="/IET/IET-logo.png"
             height={68}
             width={68}
@@ -18,10 +18,10 @@ export default function NavBar() {
       </h3>
 
       <div className="flex">
-        <li className="mr-6 hover:text-light-orange hover:font-medium">
+        <li className="mr-6 hover:text-light-orange text-white hover:font-medium">
           <Link href="/">Home</Link>
         </li>
-        <li className="mr-6 hover:text-light-orange hover:font-medium">
+        <li className="mr-6 hover:text-light-orange  text-white hover:font-medium">
           <Link href="/about">About</Link>
         </li>
       </div>
