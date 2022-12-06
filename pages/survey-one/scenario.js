a135d17fbfe19ac4baf2cac989f1fadb6ea0c5f0
Abbbyccc <abbychien@hotmail.com>
2022-12-06 15:59:05 +0000

    survey 1 & 2 scenario page css

--- pages/survey-one/scenario.js
+++ pages/survey-one/scenario.js
@@ -9,48 +9,56 @@ export default function Scenario() {
   return (
     <>
       <HeadComp title={"Survey I Scenario"} />
-      <main>
-        <h1>Scenario - Police</h1>
+      <main className="flex flex-col items-center justify-center h-screen">
+        <div className="p-20 bg-dim-gray max-w-5xl rounded-md ">
+          <h1 className="text-center mb-2">Scenario I - Police</h1>
 
-        {showTranscript ? (
-          <div>
-            <ScenarioTranscript />
-            <ButtonCom
-              btnName={"Start Survey"}
-              btnLink="questions" // SENDS US TO QUESTIONS
-            />
+          {showTranscript ? (
+            <div className="flex flex-col items-center justify-center">
+              <div className="py-5">
+                <ScenarioTranscript />
+              </div>
+              <div className="flex items-center justify-center [&>*]:mr-3 mt-3">
+                <ButtonCom
+                  btnName={"Start Survey"}
+                  btnLink="questions" // SENDS US TO QUESTIONS
+                />
 
-            <ButtonCom
-              btnName={"See Video"}
-              BtnOnClick={() => {
-                setTranscript(false);
-              }}
-            />
-          </div>
-        ) : (
-          <div>
-            <iframe
-              src="https://geo.dailymotion.com/player/xaygu.html?video=x8fx226"
-              allow="autoplay; fullscreen; picture-in-picture"
-              allowfullscreen
-              frameborder="0"
-              width="640"
-              height="360"
-              className="pointer-events-none"
-            ></iframe>
-            <ButtonCom
-              btnName={"Start Survey"}
-              btnLink="questions" // SENDS US TO QUESTIONS
-            />
-
-            <ButtonCom
-              btnName={"See Transcript"}
-              BtnOnClick={() => {
-                setTranscript(true);
-              }}
-            />
-          </div>
-        )}
+                <ButtonCom
+                  btnName={"See Video"}
+                  BtnOnClick={() => {
+                    setTranscript(false);
+                  }}
+                />
+              </div>
+            </div>
+          ) : (
+            <div>
+              <iframe
+                src="https://geo.dailymotion.com/player/xaygu.html?video=x8fx226"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowfullscreen
+                frameborder="0"
+                width="640"
+                height="360"
+                className="pointer-events-none"
+              ></iframe>
+              <div className="flex items-center justify-center [&>*]:mr-3 mt-3">
+                <ButtonCom
+                  btnName={"Start Survey"}
+                  btnLink="questions" // SENDS US TO QUESTIONS
+                  className="mr-1"
+                />
+                <ButtonCom
+                  btnName={"See Transcript"}
+                  BtnOnClick={() => {
+                    setTranscript(true);
+                  }}
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </main>
     </>
   );
--- pages/survey-two/scenario.js
+++ pages/survey-two/scenario.js
@@ -17,48 +17,58 @@ export default function Scenario() {
   return (
     <>
       <HeadComp title={"Survey II Scenario"} />
-      <main>
-        <h1>Same Scenario - Police - from PART I</h1>
+      <main className="flex flex-col items-center justify-center h-screen">
+        <div className="p-20 bg-dim-gray max-w-5xl rounded-md ">
+          <h1 className="text-center mb-2">
+            Scenario II (same from PART I) - Police
+          </h1>
 
-        {showTranscript ? (
-          <div>
-            <ScenarioTranscript />
-            <ButtonCom
-              btnName={"Start Survey"}
-              btnLink="questions" // SENDS US TO QUESTIONS
-            />
+          {showTranscript ? (
+            <div className="flex flex-col items-center justify-center">
+              <div className="py-5">
+                <ScenarioTranscript />
+              </div>
+              <div className="flex items-center justify-center [&>*]:mr-3 mt-3">
+                <ButtonCom
+                  btnName={"Start Survey"}
+                  btnLink="questions" // SENDS US TO QUESTIONS
+                />
 
-            <ButtonCom
-              btnName={"See Video"}
-              BtnOnClick={() => {
-                setTranscript(false);
-              }}
-            />
-          </div>
-        ) : (
-          <div>
-            <iframe
-              src="https://geo.dailymotion.com/player/xaygu.html?video=x8fx226"
-              allow="autoplay; fullscreen; picture-in-picture"
-              allowfullscreen
-              frameborder="0"
-              width="640"
-              height="360"
-              className="pointer-events-none"
-            ></iframe>
-            <ButtonCom
-              btnName={"Start Survey"}
-              btnLink="questions" // SENDS US TO QUESTIONS
-            />
+                <ButtonCom
+                  btnName={"See Video"}
+                  BtnOnClick={() => {
+                    setTranscript(false);
+                  }}
+                />
+              </div>
+            </div>
+          ) : (
+            <div>
+              <iframe
+                src="https://geo.dailymotion.com/player/xaygu.html?video=x8fx226"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowfullscreen
+                frameborder="0"
+                width="640"
+                height="360"
+                className="pointer-events-none"
+              ></iframe>
+              <div className="flex items-center justify-center [&>*]:mr-3 mt-3">
+                <ButtonCom
+                  btnName={"Start Survey"}
+                  btnLink="questions" // SENDS US TO QUESTIONS
+                />
 
-            <ButtonCom
-              btnName={"See Transcript"}
-              BtnOnClick={() => {
-                setTranscript(true);
-              }}
-            />
-          </div>
-        )}
+                <ButtonCom
+                  btnName={"See Transcript"}
+                  BtnOnClick={() => {
+                    setTranscript(true);
+                  }}
+                />
+              </div>
+            </div>
+          )}
+        </div>
       </main>
     </>
   );
