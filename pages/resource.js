0af3f1818ce099244e8be88bea0502f535188318
Suraj Pun <s_pun@outlook.com>
2022-12-06 16:06:24 +0000

    styled resources page

--- pages/resource.js
+++ pages/resource.js
@@ -10,50 +10,61 @@ export default function Resource() {
       <HeadComp title={"IET Resources"} />
       <NavBar />
       <main>
-        <h1> Feedback Page</h1>
-        <Image
-          src="/IET/iceberg.jpg"
-          alt="image of icerberg"
-          height={600}
-          width={600}
-        />
-        <iframe
-        src="https://airtable.com/embed/shrkiPRE11Zoj3rc5?backgroundColor=green"
-        width="100%"
-        height="533"
-      ></iframe>
+        <div className="flex justify-center mt-20 relative">
+          <div>
+            <Image
+              className="rounded-lg"
+              src="/IET/iceberg.jpg"
+              alt="image of icerberg"
+              height={700}
+              width={550}
+            />
 
-        <p>Additional resources Links</p>
-        <ol>
-          <li>
-            {" "}
-            <Link href="https://rb.gy/uc0jnw">
-              Stop & search rate by ethnicity April 2020 to March 2021
-            </Link>
-          </li>
-          <li>
-            <Link href="https://rb.gy/nsijfo">
-              Timeline: Race and the Metropolitan police
-            </Link>
-          </li>
-          <li>
-            <Link href="https://rb.gy/m5dikr">
-              How to Reduce Unconscious Bias in the Workplace
-            </Link>
-          </li>
-          <li>
-            <Link href="https://rb.gy/okgtoi">
-              What Are the Benefits of Understanding Different Cultures?
-            </Link>
-          </li>
-          <li>
-            <Link href="https://rb.gy/4zvwle">
-              How Do You Reduce Prejudice?
-            </Link>
-          </li>
-        </ol>
+            <div className="ml-10 mt-80 text-white text-justify leading-8 absolute top-0 text-sm">
+              <h2 className="text-4xl font-bold mt-10">
+                Additional resources Links
+              </h2>
+              <ol className="text-grey text-1xl mt-10 text-lg leading-10">
+                <li className="hover:text-light-orange">
+                  {" "}
+                  <Link href="https://rb.gy/uc0jnw">
+                    Stop & search rate by ethnicity April 2020 to March 2021
+                  </Link>
+                </li>
+                <li className="hover:text-light-orange">
+                  <Link href="https://rb.gy/nsijfo">
+                    Timeline: Race and the Metropolitan police
+                  </Link>
+                </li>
+                <li className="hover:text-light-orange">
+                  <Link href="https://rb.gy/m5dikr">
+                    How to Reduce Unconscious Bias in the Workplace
+                  </Link>
+                </li>
+                <li className="hover:text-light-orange">
+                  <Link href="https://rb.gy/okgtoi">
+                    What Are the Benefits of Understanding Different Cultures?
+                  </Link>
+                </li>
+                <li className="hover:text-light-orange">
+                  <Link href="https://rb.gy/4zvwle">
+                    How Do You Reduce Prejudice?
+                  </Link>
+                </li>
+              </ol>
+            </div>
+          </div>
 
-        <ButtonCom btnName={"Home"} btnLink="/" />
+          <iframe
+            src="https://airtable.com/embed/shrkiPRE11Zoj3rc5?backgroundColor=green"
+            height={730}
+            width={550}
+          ></iframe>
+        </div>
+
+        <span className="flex justify-center mt-5">
+          <ButtonCom btnName={"Home"} btnLink="/" />
+        </span>
       </main>
     </>
   );
