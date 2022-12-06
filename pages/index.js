a5c9623b24d495fe0a28acc8deef82a7c89f94ca
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-12-06 14:52:43 +0000

    home page stying

--- pages/index.js
+++ pages/index.js
@@ -14,59 +14,81 @@ export default function Home() {
       <HeadComp title={"Home-IET"} />
 
       <NavBar />
-      <main>
+      <main className="flex flex-col items-center justify-center">
         <Image
-          className="invert"
+          className="invert py-10"
           src="/IET/IET-logo.png"
-          height={250}
-          width={250}
+          height={200}
+          width={200}
           alt="IET logo"
         />
-        <h1 className="text-7xl text-center">Immersive Empathy Training</h1>
-        <ButtonCom
-          btnName={"Start"}
-          BtnOnClick={() => {
-            setShowModal(true);
-          }}
-        />
+
+        <h1 className="text-7xl text-center text-white">
+          Immersive Empathy Training
+        </h1>
+
+        <div className="pt-20" type="button" data-modal-toggle="popup-modal">
+          <ButtonCom
+            btnName={"START"}
+            BtnOnClick={() => {
+              setShowModal(true);
+            }}
+          />
+        </div>
+
         {/* Modal */}
         {showModal && (
-          <div>
-            <h2 className="text-5xl">
+          <div className="absolute bottom-10 left-auto p-10 bg-dim-gray max-w-7xl rounded-md">
+            <div className="absolute right-2 top-2">
+              <ButtonCom btnName={"X"} BtnOnClick={() => setShowModal(false)} />
+            </div>
+
+            <h2 className="text-5xl pb-4 text-center">
               Introduction to Immersive Empathy Training
             </h2>
+
             {text ? (
-              <>
-                <IntroToTraining />
-                <ButtonCom
-                  btnName={"Watch video instead"}
-                  BtnOnClick={() => setText(false)}
-                />
-              </>
+              <div>
+                <div className="max-w-4xl">
+                  <IntroToTraining />
+                </div>
+
+                <div className="text-center pb-4 pt-3">
+                  <ButtonCom
+                    btnName={"Watch video instead"}
+                    BtnOnClick={() => setText(false)}
+                  />
+                </div>
+              </div>
             ) : (
               <>
-                <iframe
-                  src="https://geo.dailymotion.com/player/xaygu.html?video=x8fx1n1"
-                  allow="autoplay; fullscreen; picture-in-picture"
-                  allowfullscreen
-                  frameborder="0"
-                  width="640"
-                  height="360"
-                  className="pointer-events-none"
-                ></iframe>
+                <div className="grid place-items-center pb-4">
+                  <iframe
+                    src="https://geo.dailymotion.com/player/xaygu.html?video=x8fx1n1"
+                    allow="autoplay; fullscreen; picture-in-picture"
+                    allowfullscreen
+                    frameborder="0"
+                    width="640"
+                    height="360"
+                    className="pointer-events-none"
+                  ></iframe>
+                </div>
 
-                <ButtonCom
-                  btnName={"Read transcript instead"}
-                  BtnOnClick={() => setText(true)}
-                />
+                <div className="text-center pb-4">
+                  <ButtonCom
+                    btnName={"Read transcript instead"}
+                    BtnOnClick={() => setText(true)}
+                  />
+                </div>
               </>
             )}
 
-            <ButtonCom
-              btnLink="/survey-one/introduction"
-              btnName={"Acknowledged"}
-            />
-            <ButtonCom btnName={"X"} BtnOnClick={() => setShowModal(false)} />
+            <div className="text-center">
+              <ButtonCom
+                btnLink="/survey-one/introduction"
+                btnName={"Acknowledged"}
+              />
+            </div>
           </div>
         )}
       </main>
