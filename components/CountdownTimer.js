a3858ad0fd03ae0d4b106f1317adfc8d240b8877
Suraj Pun <s_pun@outlook.com>
2022-12-06 12:20:13 +0000

    Survey One Time is up & Show next button after input

--- components/CountdownTimer.js
+++ components/CountdownTimer.js
@@ -3,10 +3,26 @@ import { useEffect, useState } from "react";
 
 export default function CountdownTimer({ sec }) {
   const [counter, setCounter] = useState(sec);
+  const [completed, setCompleted] = useState(false);
 
   useEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    if (counter === 0) {
+      setCompleted(true);
+    }
   }, [counter]);
 
-  return <div> Countdown {counter} </div>;
+  return (
+    <>
+      <div> Countdown {counter} </div>
+
+      {/* Time up Modal */}
+      {completed && (
+        <div>
+          <audio hidden autoPlay controls src="/audio/TimeUp.wav" />
+          Time is Up, Please continue the exercise...
+        </div>
+      )}
+    </>
+  );
 }
