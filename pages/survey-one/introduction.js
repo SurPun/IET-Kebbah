de4370afe8b64cd81323d4148c4ecb4f499436ed
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-12-06 15:29:56 +0000

    survey 1 intro page

--- pages/survey-one/introduction.js
+++ pages/survey-one/introduction.js
@@ -5,15 +5,17 @@ export default function Introduction() {
   return (
     <>
       <HeadComp title={"Survey I Intro"} />
-      <main>
-        <h1>PART ONE</h1>
-        <p>Survey I</p>
-        <p>Police View Scenario</p>
-        <p>
-          You will be given 5 seconds to read a question and then can type your
-          answer or speak into the microphone
-        </p>
-        <ButtonCom btnName={"To Survey"} btnLink="scenario" />
+      <main className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col rounded-lg bg-dim-gray items-center p-10 [&>*]:pb-3">
+          <h1>PART ONE</h1>
+          <p>Survey I</p>
+          <p>Police View Scenario</p>
+          <p className="text-red-600">
+            You will be given 5 seconds to read a question and then can type
+            your answer or speak into the microphone
+          </p>
+          <ButtonCom btnName={"To Survey"} btnLink="scenario" />
+        </div>
       </main>
     </>
   );
