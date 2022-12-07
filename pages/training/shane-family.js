ce68c466362bc999da4c35e5c93c53d0e88529ce
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-12-07 17:19:35 +0000

    option-fixes

--- pages/training/shane-family.js
+++ pages/training/shane-family.js
@@ -23,24 +23,27 @@ export default function ShaneFamily() {
           <h2 className="text-2xl mt-6 mb-2 text-white">Hello, I am Shane</h2>
           <div className="flex list-none justify-center">
             <Image
+              className="rounded-md"
               src="/portraits/shane.jpg"
               alt="image of Shane"
               height={230}
               width={230}
             />
           </div>
-          <p className="text-xl my-1 text-white">This is my family</p>
 
           <p className="text-xl my-1 text-white">
             You will see what it is like to be me...
           </p>
-          <div className="mt-3 pb-4">
+          <div className="py-8">
             <ButtonCom btnName={"Become me..."} btnLink={"shane-life"} />
           </div>
+
+          <p className="text-xl my-1 text-white">This is my family</p>
 
           <ul className="flex list-none text-center justify-around">
             <li>
               <Image
+                className="rounded-md"
                 src="/portraits/dad.jpg"
                 alt="image of Shane's dad"
                 height={230}
@@ -50,6 +53,7 @@ export default function ShaneFamily() {
             </li>
             <li>
               <Image
+                className="rounded-md"
                 src="/portraits/mum.jpg"
                 alt="image of Shane's mom"
                 height={230}
@@ -59,12 +63,13 @@ export default function ShaneFamily() {
             </li>
             <li>
               <Image
+                className="rounded-md"
                 src="/portraits/shanice.jpg"
                 alt="image of Shane's sister"
                 height={230}
                 width={230}
               />
-              <p className="text-xl my-1 text-white">Shanice</p>
+              <p className="text-xl my-1 text-white">Shanice (sister) </p>
             </li>
           </ul>
         </div>
