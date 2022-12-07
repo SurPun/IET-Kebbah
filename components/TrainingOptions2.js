2004c7ed4cff5134f01445aa382f31be63463be2
Suraj Pun <s_pun@outlook.com>
2022-12-07 12:23:26 +0000

    Styled Decision Modal & fix resources page img

--- components/TrainingOptions2.js
+++ components/TrainingOptions2.js
@@ -11,20 +11,25 @@ export default function TrainingOptions2() {
     const timer = setTimeout(() => {
       setShowModal(true);
     }, 78000);
+    // 78000
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <>
-      {!showModal ? (
+      {showModal ? (
         <>
-          <div className="p-20 backdrop-blur-3xl max-w-5xl rounded-md absolute top-20">
+          <div className="min-h-screen p-40 backdrop-blur-3xl rounded-md absolute left-0 right-0 bottom-0 text-white">
+            <h2 className="text-center font-bold mb-20 text-6xl">
+              Will you Accept or Decline?
+            </h2>
             <div className="flex justify-center">⌛</div>
-            <div className="flex justify-center gap-10">
+
+            <div className="flex flex-col items-center justify-center mt-5">
               <CountdownTimer sec={30} />
             </div>
 
-            <div className="flex justify-center gap-20">
+            <div className="flex justify-center gap-10 mt-8">
               {/* Accept */}
               <ButtonCom btnName={"Accept"} btnLink="./shane-end" />
 
